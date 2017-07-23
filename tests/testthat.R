5ab2ccb71d5b8f0383527bca821679cf4da8a7c5
Yuya Matsumura <matsumurayuuya@yuya-no-MacBook-Pro.local>
2017-07-23 18:53:23 +0900

    add test-jum_wakati

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rjumanpp)
+
+test_check("rjumanpp")
---
+++ tests/testthat/test-jum_wakati.R
@@ -0,0 +1,26 @@
+context("simple wakatigaki")
+
+test_that("wakatigaki", {
+  res <- jum_wakati("私はミルクティを飲みます")
+  wakati <- "私 は ミルクティ を 飲み ます"
+  expect_equal(res, wakati)
+})
+
+test_that("wakatigaki pos", {
+  res <- jum_wakati("私はミルクティを飲みます", pos = "名詞")
+  wakati <- "私 ミルクティ"
+  expect_equal(res, wakati)
+})
+
+test_that("wakatigaki redirect", {
+  res <- jum_wakati("私はミルクティを飲みます", redirect = TRUE)
+  wakati <- "私 は ミルクティー を 飲み ます"
+
+
+  res2 <- jum_wakati("私はミルクティを飲みます", pos = "名詞", redirect = TRUE)
+  wakati2 <- "私 ミルクティー"
+
+  expect_equal(res, wakati)
+})
+
+
