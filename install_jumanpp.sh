8ae4f6ccf43255cd059c3e15d9e338c16de19aa6
Yuya Matsumura <matsumurayuuya@yuya-no-MacBook-Pro.local>
2017-07-25 23:33:41 +0900

    fix travis

--- install_jumanpp.sh
+++ install_jumanpp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# boost
+sudo wget -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download
+sudo tar xzvf boost_1_64_0.tar.gz
+cd boost_1_64_0/
+sudo ./bootstrap.sh --prefix=/usr/local --with-libraries=program_options,filesystem,system
+sudo ./b2 link=shared threading=multi variant=release
+sudo ./b2 install
+
 # JUMAN++
 cd Downloads
 wget http://lotus.kuee.kyoto-u.ac.jp/nl-resource/jumanpp/jumanpp-1.02.tar.xz
