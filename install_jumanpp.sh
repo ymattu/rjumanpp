a3f678deecc83211f24c27fc9b21f139326b423a
Yuya Matsumura <matsumura@pudding.local>
2017-07-26 13:39:27 +0900

    fix travis

--- install_jumanpp.sh
+++ install_jumanpp.sh
@@ -6,12 +6,13 @@ sudo apt-get install gcc-4.9 google-perftools libgoogle-perftools-dev
 wget http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2
 tar xjf ./boost_1_57_0.tar.bz2
 cd boost_1_57_0
-./bootstrap.sh --prefix=/opt/boost_1_57_0
-sudo ./b2
-sudo ./b2 install -j2
+./bootstrap.sh
+sudo ./b2 toolset=gcc-4.9
+sudo ./b2 install -j2 --prefix=/opt/boost_1_57_0
 
 # JUMAN++
 mkdir Downloads
+cd Downloads
 wget http://lotus.kuee.kyoto-u.ac.jp/nl-resource/jumanpp/jumanpp-1.01.tar.xz
 tar xJvf jumanpp-1.01.tar.xz
 cd jumanpp-1.01
