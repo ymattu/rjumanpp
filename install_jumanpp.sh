32910a5851e519a7446f10969326f706fff41cef
Yuya Matsumura <matsumura@pudding.local>
2017-07-26 14:15:38 +0900

    fix travis

--- install_jumanpp.sh
+++ install_jumanpp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Boost 1.57
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get install g++-4.9
 sudo apt-get update -qq
